2865066ff5a136be063d6154e37674f6ccc3b831
hytham defrawy <hythamdefrawy@gmail.com>
2021-11-03 17:06:27 -0400

    Modified the function so you can search word horezintally & vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    if (!letters.length) {
+        return false;
+    };
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true
+        }
     }
-}
+    let vertical = [];
+    for (let i = 0; i < letters[i].length; i++) {
+        vertical = [];
+        for (j = 0; j < letters.length; j++) {
+            vertical.push(letters[j][i]);
+            if (vertical.join('').includes(word)) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
